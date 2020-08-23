e383e4086061a4aad1374fdf1c4cc1cf6b64d994
datmv <42610120+datmv123@users.noreply.github.com>
2020-08-23 22:37:34 +0700

    update view comment review

--- components/exercise/CommentsSection.js
+++ components/exercise/CommentsSection.js
@@ -107,13 +107,13 @@ const CommentsSection = ({
           )}>
           {isSelfReview && (
             <Panel header={`Reviewer: You (${userInfo.email})`} key=''>
-              <Descriptions size='small' column={1}>
-                <Descriptions.Item label='Type'>Self-Review</Descriptions.Item>
-                <Descriptions.Item
-                  label='Status'
-                  style={{
-                    textTransform: 'capitalize',
-                  }}>
+              <Row style={{ marginBottom: '0.5em' }}>
+                <Col style={{ width: 90, color: '#000000D9' }}>Type:</Col>
+                <Col>Self-Review</Col>
+              </Row>
+              <Row style={{ marginBottom: '0.5em' }}>
+                <Col style={{ width: 90, color: '#000000D9' }}>Status:</Col>
+                <Col>
                   {reviewInfos.isAccepted === 'rejected' && (
                     <span style={{ color: 'red' }}>Rejected</span>
                   )}
@@ -121,52 +121,87 @@ const CommentsSection = ({
                     <span style={{ color: 'green' }}>Accepted</span>
                   )}
                   {reviewInfos.isAccepted === 'waiting' && <span>Waiting</span>}
-                </Descriptions.Item>
-                <Descriptions.Item label='ReviewedAt'>
-                  {formatDate(moment(reviewInfos.updatedAt).toDate())}
-                </Descriptions.Item>
-                <Descriptions.Item label='Content'>
-                  {reviewInfos.selfComment}
-                </Descriptions.Item>
-              </Descriptions>
+                </Col>
+              </Row>
+              <Row style={{ marginBottom: '0.5em' }}>
+                <Col style={{ width: 90, color: '#000000D9' }}>ReviewedAt:</Col>
+                <Col>{formatDate(moment(reviewInfos.updatedAt).toDate())}</Col>
+              </Row>
+              <Row style={{ marginBottom: '0.5em' }}>
+                <Col style={{ width: 90, color: '#000000D9' }}>Content:</Col>
+                <Col style={{ flexGrow: 1 }}>
+                  <pre
+                    style={{
+                      marginBottom: 0,
+                      overflow: 'scroll',
+                      minHeight: 80,
+                      maxHeight: 200,
+                      backgroundColor: 'rgb(246, 248, 250)',
+                      paddingLeft: '1em',
+                      paddingTop: '0.5em',
+                    }}>
+                    {reviewInfos.selfComment}
+                  </pre>
+                </Col>
+              </Row>
             </Panel>
           )}
           {reviewInfos.details.map((t) => {
-            let reviewer = t.reviewer;
+            let reviewer = t.reviewer || {};
             let header =
               userInfo.email === reviewer.email
                 ? `Reviewer: You (${userInfo.email})`
                 : `Reviewer: ${reviewer.email}`;
             return (
-              <Panel header={header} key=''>
-                <Descriptions size='small' column={1}>
-                  <Descriptions.Item label='Type'>
-                    Requested Review
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label='Status'
-                    style={{
-                      textTransform: 'capitalize',
-                    }}>
-                    {t.isAccepted === 'rejected' && (
+              <Panel header={header} key={header}>
+                <Descriptions size='small' column={1}></Descriptions>
+                <Row style={{ marginBottom: '0.5em' }}>
+                  <Col style={{ width: 90, color: '#000000D9' }}>Type:</Col>
+                  <Col>Requested Review</Col>
+                </Row>
+                <Row style={{ marginBottom: '0.5em' }}>
+                  <Col style={{ width: 90, color: '#000000D9' }}>Status:</Col>
+                  <Col>
+                    {reviewInfos.isAccepted === 'rejected' && (
                       <span style={{ color: 'red' }}>Rejected</span>
                     )}
-                    {t.isAccepted === 'accepted' && (
+                    {reviewInfos.isAccepted === 'accepted' && (
                       <span style={{ color: 'green' }}>Accepted</span>
                     )}
-                    {t.isAccepted === 'waiting' && <span>Waiting</span>}
-                  </Descriptions.Item>
-                  {t.isAccepted !== 'waiting' && (
-                    <>
-                      <Descriptions.Item label='ReviewedAt'>
-                        {formatDate(moment(t.updatedAt).toDate())}
-                      </Descriptions.Item>
-                      <Descriptions.Item label='Content'>
-                        {t.comment}
-                      </Descriptions.Item>
-                    </>
-                  )}
-                </Descriptions>
+                    {reviewInfos.isAccepted === 'waiting' && (
+                      <span>Waiting</span>
+                    )}
+                  </Col>
+                </Row>
+                {t.isAccepted !== 'waiting' && (
+                  <>
+                    <Row style={{ marginBottom: '0.5em' }}>
+                      <Col style={{ width: 90, color: '#000000D9' }}>
+                        ReviewedAt:
+                      </Col>
+                      <Col>{formatDate(moment(t.updatedAt).toDate())}</Col>
+                    </Row>
+                    <Row style={{ marginBottom: '0.5em' }}>
+                      <Col style={{ width: 90, color: '#000000D9' }}>
+                        Content:
+                      </Col>
+                      <Col style={{ flexGrow: 1 }}>
+                        <pre
+                          style={{
+                            marginBottom: 0,
+                            overflow: 'scroll',
+                            minHeight: 80,
+                            maxHeight: 200,
+                            backgroundColor: 'rgb(246, 248, 250)',
+                            paddingLeft: '1em',
+                            paddingTop: '0.5em',
+                          }}>
+                          {t.comment}
+                        </pre>
+                      </Col>
+                    </Row>
+                  </>
+                )}
               </Panel>
             );
           })}
